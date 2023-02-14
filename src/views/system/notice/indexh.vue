4a1a1ab297834241979b2c42e70baaf4dbc4f12f
xingyuv <xingyu4j@vip.qq.com>
2023-02-14 15:48:23 +0800

    refactor: table-v2

--- src/views/system/notice/indexh.vue
+++ src/views/system/notice/indexh.vue
@@ -1,20 +1,5 @@
 <template>
   <ContentWrap>
-    <el-form ref="searchForm" :model="queryParms" :inline="true">
-      <el-form-item label="公告标题">
-        <el-input v-model="queryParms.title" />
-      </el-form-item>
-      <el-form-item label="状态">
-        <el-select v-model="queryParms.status">
-          <el-option label="全部" value="" />
-          <el-option label="开启" :value="1" />
-          <el-option label="关闭" :value="0" />
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="getList">Query</el-button>
-      </el-form-item>
-    </el-form>
     <div style="width: 100%; height: 500px">
       <el-auto-resizer>
         <template #default="{ height, width }">
@@ -32,9 +17,8 @@
     </div>
   </ContentWrap>
 </template>
-<script setup lang="tsx">
-import dayjs from 'dayjs'
-import { Column, ElPagination, ElTableV2, TableV2FixedDir } from 'element-plus'
+<script setup lang="ts">
+import { Column, TableV2FixedDir } from 'element-plus'
 import * as NoticeApi from '@/api/system/notice'
 import { XTextButton } from '@/components/XButton'
 const { t } = useI18n() // 国际化
@@ -69,17 +53,13 @@ const columns: Column<any>[] = [
     key: 'content',
     dataKey: 'content',
     title: '公告内容',
-    width: 400,
-    cellRenderer: ({ cellData: content }) => <span v-html={content}></span>
+    width: 180
   },
   {
     key: 'createTime',
     dataKey: 'createTime',
     title: t('common.createTime'),
-    width: 180,
-    cellRenderer: ({ cellData: createTime }) => (
-      <>{dayjs(createTime).format('YYYY-MM-DD HH:mm:ss')}</>
-    )
+    width: 180
   },
   {
     key: 'actionbtns',
@@ -88,13 +68,13 @@ const columns: Column<any>[] = [
     width: 80, //当前列的宽度，必须设置
     fixed: TableV2FixedDir.RIGHT, //是否固定列
     align: 'center',
-    cellRenderer: ({ cellData: id }) => (
-      <XTextButton
-        preIcon="ep:delete"
-        title={t('action.del')}
-        onClick={handleDelete.bind(this, id)}
-      ></XTextButton>
-    )
+    cellRenderer: (date) =>
+      h(XTextButton, {
+        onClick: () => handleDelete(date.rowData),
+        type: 'danger',
+        preIcon: 'ep:delete',
+        title: t('action.del')
+      })
   }
 ]
 
@@ -115,8 +95,8 @@ const getList = async () => {
   tableTotal.value = res.total
 }
 
-const handleDelete = (id) => {
-  console.info(id)
+const handleDelete = (row) => {
+  console.info(row.id)
 }
 
 getList()
