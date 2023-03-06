cbc21c33b1861414d0422784da2d1a5a66de80d9
puhui999 <puhui999@163.com>
2023-03-06 11:48:39 +0800

    1、解决自定义字典标签值为零时标签不渲染的问题 2、添加标签的文字颜色为白色，解决自定义背景颜色时标签文字看不清的问题

--- src/components/DictTag/src/DictTag.vue
+++ src/components/DictTag/src/DictTag.vue
@@ -33,12 +33,15 @@ export default defineComponent({
       if (!props.type) {
         return null
       }
-      if (!props.value) {
+      // 解决自定义字典标签值为零时标签不渲染的问题
+      if (!props.value && props.value !== 0) {
         return null
       }
       getDictObj(props.type, props.value.toString())
+      // 添加标签的文字颜色为白色，解决自定义背景颜色时标签文字看不清的问题
       return (
         <ElTag
+          style={dictData.value?.cssClass ? 'color: #fff' : ''}
           type={dictData.value?.colorType}
           color={
             dictData.value?.cssClass && isHexColor(dictData.value?.cssClass)
