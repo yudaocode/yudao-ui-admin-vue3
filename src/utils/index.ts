11d4e2ea177a5c3d1522448e1eb2ab45eab54052
YunaiV <zhijiantianya@gmail.com>
2023-03-15 20:35:18 +0800

    补全 fileSizeFormatter 的注释

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -138,9 +138,18 @@ export const generateUUID = () => {
   })
 }
 
-export const fileSizeFormatter = (row) => {
+/**
+ * element plus 的文件大小 Formatter 实现
+ *
+ * @param row 行数据
+ * @param column 字段
+ * @param cellValue 字段值
+ */
+// @ts-ignore
+export const fileSizeFormatter = (row, column, cellValue) => {
+  const fileSize = cellValue
   const unitArr = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  const srcSize = parseFloat(row.size)
+  const srcSize = parseFloat(fileSize)
   const index = Math.floor(Math.log(srcSize) / Math.log(1024))
   const size = srcSize / Math.pow(1024, index)
   const sizeStr = size.toFixed(2) //保留的小数位数
