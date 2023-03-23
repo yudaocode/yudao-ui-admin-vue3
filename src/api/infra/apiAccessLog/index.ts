aad57e2840b02f5c5a096e3085a28a0ea8fde8fc
dtsz <314942997@qq.com>
2023-03-23 13:21:38 +0800

    访问日志重构

--- src/api/infra/apiAccessLog/index.ts
+++ src/api/infra/apiAccessLog/index.ts
@@ -18,33 +18,12 @@ export interface ApiAccessLogVO {
   resultMsg: string
   createTime: Date
 }
-
-export interface ApiAccessLogPageReqVO extends PageParam {
-  userId?: number
-  userType?: number
-  applicationName?: string
-  requestUrl?: string
-  beginTime?: Date[]
-  duration?: number
-  resultCode?: number
-}
-
-export interface ApiAccessLogExportReqVO {
-  userId?: number
-  userType?: number
-  applicationName?: string
-  requestUrl?: string
-  beginTime?: Date[]
-  duration?: number
-  resultCode?: number
-}
-
 // 查询列表API 访问日志
-export const getApiAccessLogPageApi = (params: ApiAccessLogPageReqVO) => {
+export const getApiAccessLogPage = (params: PageParam) => {
   return request.get({ url: '/infra/api-access-log/page', params })
 }
 
 // 导出API 访问日志
-export const exportApiAccessLogApi = (params: ApiAccessLogExportReqVO) => {
+export const exportApiAccessLog = (params) => {
   return request.download({ url: '/infra/api-access-log/export-excel', params })
 }
