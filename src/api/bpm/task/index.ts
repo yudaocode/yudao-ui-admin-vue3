c1e760da6dfdbc0e1bcc04b5a758948e1a428ad3
Chika <wbs_2018@sina.com>
2023-04-04 09:29:54 +0800

    流程-已办任务路由中增加详情Model

--- src/api/bpm/task/index.ts
+++ src/api/bpm/task/index.ts
@@ -1,5 +1,9 @@
 import request from '@/config/axios'
 
+export type TaskVO = {
+  id: number
+}
+
 export const getTodoTaskPage = async (params) => {
   return await request.get({ url: '/bpm/task/todo-page', params })
 }
@@ -31,4 +35,9 @@ export const getTaskListByProcessInstanceId = async (processInstanceId) => {
   return await request.get({
     url: '/bpm/task/list-by-process-instance-id?processInstanceId=' + processInstanceId
   })
+}
+
+// 导出任务
+export const exportTask = async (params) => {
+  return await request.download({ url: '/bpm/task/export', params })
 }
