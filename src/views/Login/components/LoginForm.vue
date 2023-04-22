c14b5c5bff083f7289ed8e955cd74e1e9a961120
YunaiV <zhijiantianya@gmail.com>
2023-04-22 21:21:45 +0800

    登录界面，增加“萌新必读”

--- src/views/Login/components/LoginForm.vue
+++ src/views/Login/components/LoginForm.vue
@@ -125,6 +125,21 @@
           </div>
         </el-form-item>
       </el-col>
+      <el-divider content-position="center">萌新必读</el-divider>
+      <el-col :span="24" style="padding-left: 10px; padding-right: 10px">
+        <el-form-item>
+          <div class="flex justify-between w-[100%]">
+            <el-link href="https://doc.iocoder.cn/" target="_blank">📚开发指南</el-link>
+            <el-link href="https://doc.iocoder.cn/video/" target="_blank">🔥视频教程</el-link>
+            <el-link href="https://www.iocoder.cn/Interview/good-collection/" target="_blank">
+              ⚡面试手册
+            </el-link>
+            <el-link href="http://static.yudao.iocoder.cn/mp/Aix9975.jpeg" target="_blank">
+              🤝外包咨询
+            </el-link>
+          </div>
+        </el-form-item>
+      </el-col>
     </el-row>
   </el-form>
 </template>
