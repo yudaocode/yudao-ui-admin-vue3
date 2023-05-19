9f0065f89f312b4003f2b2154728a537c499c7b6
xingyu <xingyu4j@vip.qq.com>
2023-05-19 15:37:01 +0800

    fix: echart color

--- src/components/Echart/src/Echart.vue
+++ src/components/Echart/src/Echart.vue
@@ -27,7 +27,7 @@ const props = defineProps({
 const isDark = computed(() => appStore.getIsDark)
 
 const theme = computed(() => {
-  const echartTheme: boolean | string = unref(isDark) ? true : 'inherit'
+  const echartTheme: boolean | string = unref(isDark) ? true : 'auto'
 
   return echartTheme
 })
