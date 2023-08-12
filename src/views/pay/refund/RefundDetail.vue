10c121d62d622b3b3d46af3210170a1e1b103c99
dhb52 <dhb52@126.com>
2023-08-13 01:53:08 +0800

    fix: 修复ElDescriptions对齐问题

--- src/views/pay/refund/RefundDetail.vue
+++ src/views/pay/refund/RefundDetail.vue
@@ -9,20 +9,17 @@
           refundDetail.channelRefundNo
         }}</el-tag>
       </el-descriptions-item>
-    </el-descriptions>
-    <el-descriptions :column="2" label-class-name="desc-label">
+
       <el-descriptions-item label="商户支付单号">
         <el-tag size="small">{{ refundDetail.merchantOrderId }}</el-tag>
       </el-descriptions-item>
       <el-descriptions-item label="渠道支付单号">
         <el-tag type="success" size="small">{{ refundDetail.channelOrderNo }}</el-tag>
       </el-descriptions-item>
-    </el-descriptions>
-    <el-descriptions :column="2" label-class-name="desc-label">
+
       <el-descriptions-item label="应用编号">{{ refundDetail.appId }}</el-descriptions-item>
       <el-descriptions-item label="应用名称">{{ refundDetail.appName }}</el-descriptions-item>
-    </el-descriptions>
-    <el-descriptions :column="2" label-class-name="desc-label">
+
       <el-descriptions-item label="支付金额">
         <el-tag type="success" size="small"
           >￥{{ (refundDetail.payPrice / 100.0).toFixed(2) }}</el-tag
@@ -33,16 +30,14 @@
           >￥{{ (refundDetail.refundPrice / 100.0).toFixed(2) }}</el-tag
         >
       </el-descriptions-item>
-    </el-descriptions>
-    <el-descriptions :column="2" label-class-name="desc-label">
+
       <el-descriptions-item label="退款状态">
         <dict-tag :type="DICT_TYPE.PAY_REFUND_STATUS" :value="refundDetail.status" />
       </el-descriptions-item>
       <el-descriptions-item label="退款时间">{{
         formatDate(refundDetail.successTime)
       }}</el-descriptions-item>
-    </el-descriptions>
-    <el-descriptions :column="2" label-class-name="desc-label">
+
       <el-descriptions-item label="创建时间">{{
         formatDate(refundDetail.createTime)
       }}</el-descriptions-item>
@@ -50,20 +45,23 @@
         formatDate(refundDetail.updateTime)
       }}</el-descriptions-item>
     </el-descriptions>
+
     <!-- 分割线 -->
     <el-divider />
+
     <el-descriptions :column="2" label-class-name="desc-label">
       <el-descriptions-item label="退款渠道">
         <dict-tag :type="DICT_TYPE.PAY_CHANNEL_CODE" :value="refundDetail.channelCode" />
       </el-descriptions-item>
       <el-descriptions-item label="退款原因">{{ refundDetail.reason }}</el-descriptions-item>
-    </el-descriptions>
-    <el-descriptions :column="2" label-class-name="desc-label">
+
       <el-descriptions-item label="退款 IP">{{ refundDetail.userIp }}</el-descriptions-item>
       <el-descriptions-item label="通知 URL">{{ refundDetail.notifyUrl }}</el-descriptions-item>
     </el-descriptions>
+
     <!-- 分割线 -->
     <el-divider />
+
     <el-descriptions :column="2" label-class-name="desc-label">
       <el-descriptions-item label="渠道错误码">{{
         refundDetail.channelErrorCode
@@ -72,6 +70,7 @@
         refundDetail.channelErrorMsg
       }}</el-descriptions-item>
     </el-descriptions>
+
     <el-descriptions :column="1" label-class-name="desc-label" direction="vertical" border>
       <el-descriptions-item label="支付通道异步回调内容">
         {{ refundDetail.channelNotifyData }}
