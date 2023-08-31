f9780b636c3f7320e71707f2d0bc9311632d68db
puhui999 <puhui999@163.com>
2023-08-31 21:13:20 +0800

    fix: 1、修复营销活动相关页面打不开的问题 2、修复 spu 添加失败的问题 2、重构 spu 的 sku 校验方式为通用配置的方式

--- src/views/mall/product/spu/components/index.ts
+++ src/views/mall/product/spu/components/index.ts
@@ -1,4 +1,5 @@
 import SkuList from './SkuList.vue'
+import { Spu } from '@/api/mall/product/spu'
 
 interface PropertyAndValues {
   id: number
@@ -22,4 +23,32 @@ interface RuleConfig {
   message: string
 }
 
-export { SkuList, PropertyAndValues, RuleConfig }
+/**
+ * 获得商品的规格列表 - 商品相关的公共函数
+ *
+ * @param spu
+ * @return PropertyAndValues 规格列表
+ */
+const getPropertyList = (spu: Spu): PropertyAndValues[] => {
+  //  直接拿返回的 skus 属性逆向生成出 propertyList
+  const properties: PropertyAndValues[] = []
+  // 只有是多规格才处理
+  if (spu.specType) {
+    spu.skus?.forEach((sku) => {
+      sku.properties?.forEach(({ propertyId, propertyName, valueId, valueName }) => {
+        // 添加属性
+        if (!properties?.some((item) => item.id === propertyId)) {
+          properties.push({ id: propertyId!, name: propertyName!, values: [] })
+        }
+        // 添加属性值
+        const index = properties?.findIndex((item) => item.id === propertyId)
+        if (!properties[index].values?.some((value) => value.id === valueId)) {
+          properties[index].values?.push({ id: valueId!, name: valueName! })
+        }
+      })
+    })
+  }
+  return properties
+}
+
+export { SkuList, PropertyAndValues, RuleConfig, getPropertyList }
