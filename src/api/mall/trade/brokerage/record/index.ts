1e2120806d26f0749b89329b0dd94f0fa5d15edc
owen <owen@evolsun.com>
2023-09-06 00:41:20 +0800

    trade: 分销业务从 member 模块 转移到 trade模块

--- src/api/mall/trade/brokerage/record/index.ts
+++ src/api/mall/trade/brokerage/record/index.ts
@@ -2,10 +2,10 @@ import request from '@/config/axios'
 
 // 查询佣金记录列表
 export const getBrokerageRecordPage = async (params: any) => {
-  return await request.get({ url: `/member/member-brokerage-record/page`, params })
+  return await request.get({ url: `/trade/brokerage-record/page`, params })
 }
 
 // 查询佣金记录详情
 export const getBrokerageRecord = async (id: number) => {
-  return await request.get({ url: `/member/member-brokerage-record/get?id=` + id })
+  return await request.get({ url: `/trade/brokerage-record/get?id=` + id })
 }
