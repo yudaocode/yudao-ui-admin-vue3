9a6d29b2959acb8e8b7c3849fbbf032dab9539a9
xingyu <xingyu4j@vip.qq.com>
2023-09-11 16:54:34 +0800

    fix: avatar

--- src/components/Cropper/src/CropperAvatar.vue
+++ src/components/Cropper/src/CropperAvatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="user-info-head" @click="open()">
-    <img v-if="sourceValue" :src="sourceValue" alt="avatar" class="img-circle img-lg" />
-    <img v-if="!sourceValue" :src="avatar" alt="avatar" class="img-circle img-lg" />
+    <el-avatar v-if="sourceValue" :src="sourceValue" alt="avatar" class="img-circle img-lg" />
+    <el-avatar v-if="!sourceValue" :src="avatar" alt="avatar" class="img-circle img-lg" />
     <el-button v-if="showBtn" :class="`${prefixCls}-upload-btn`" @click="open()">
       {{ btnText ? btnText : t('cropper.selectImage') }}
     </el-button>
--- src/layout/components/UserInfo/src/UserInfo.vue
+++ src/layout/components/UserInfo/src/UserInfo.vue
@@ -53,7 +53,7 @@ const toDocument = () => {
 <template>
   <ElDropdown class="custom-hover" :class="prefixCls" trigger="click">
     <div class="flex items-center">
-      <img :src="avatar" alt="" class="w-[calc(var(--logo-height)-25px)] rounded-[50%]" />
+      <ElAvatar :src="avatar" alt="" class="w-[calc(var(--logo-height)-25px)] rounded-[50%]" />
       <span class="<lg:hidden text-14px pl-[5px] text-[var(--top-header-text-color)]">
         {{ userName }}
       </span>
