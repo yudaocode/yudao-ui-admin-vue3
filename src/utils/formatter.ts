b08e0e5ab275269cb8f2e391fd9019e9a2507400
puhui999 <puhui999@163.com>
2023-10-10 16:55:53 +0800

    营销活动：完善 review 提到的问题，添加拼团列表查看弹窗

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -2,6 +2,6 @@ import { floatToFixed2 } from '@/utils'
 
 // 格式化金额【分转元】
 // @ts-ignore
-export const fenToYuanFormat = (_, _, cellValue: any, _) => {
+export const fenToYuanFormat = (_, __, cellValue: any, ___) => {
   return `￥${floatToFixed2(cellValue)}`
 }
