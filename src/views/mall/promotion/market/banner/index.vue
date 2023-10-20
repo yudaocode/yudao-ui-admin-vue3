491b3a5018db069f0237473441b1189273ee0f0d
puhui999 <puhui999@163.com>
2023-10-21 02:40:20 +0800

    新增 banner 管理

--- src/views/mall/promotion/market/banner/index.vue
+++ src/views/mall/promotion/market/banner/index.vue
@@ -6,34 +6,19 @@
       :inline="true"
       :model="queryParams"
       class="-mb-15px"
-      label-width="80px"
+      label-width="100px"
     >
-      <el-form-item label="文章分类" prop="categoryId">
-        <el-select
-          v-model="queryParams.categoryId"
-          class="!w-240px"
-          placeholder="全部"
-          @keyup.enter="handleQuery"
-        >
-          <el-option
-            v-for="item in categoryList"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="文章标题" prop="title">
+      <el-form-item label="Banner标题" prop="title">
         <el-input
           v-model="queryParams.title"
           class="!w-240px"
           clearable
-          placeholder="请输入文章标题"
+          placeholder="请输入Banner标题"
           @keyup.enter="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="状态" prop="status">
-        <el-select v-model="queryParams.status" class="!w-240px" clearable placeholder="请选择状态">
+      <el-form-item label="活动状态" prop="status">
+        <el-select v-model="queryParams.status" class="!w-240px" clearable placeholder="全部">
           <el-option
             v-for="dict in getIntDictOptions(DICT_TYPE.COMMON_STATUS)"
             :key="dict.value"
@@ -63,7 +48,7 @@
           重置
         </el-button>
         <el-button
-          v-hasPermi="['promotion:article:create']"
+          v-hasPermi="['market:banner:create']"
           plain
           type="primary"
           @click="openForm('create')"
@@ -78,37 +63,31 @@
   <!-- 列表 -->
   <ContentWrap>
     <el-table v-loading="loading" :data="list" :show-overflow-tooltip="true" :stripe="true">
-      <el-table-column align="center" label="封面" min-width="80" prop="picUrl">
+      <el-table-column align="center" label="Banner标题" prop="title" />
+      <el-table-column align="center" label="图片" min-width="80" prop="picUrl">
         <template #default="{ row }">
           <el-image :src="row.picUrl" class="h-30px w-30px" @click="imagePreview(row.picUrl)" />
         </template>
       </el-table-column>
-      <el-table-column align="center" label="标题" min-width="180" prop="title" />
-      <el-table-column align="center" label="分类" min-width="180" prop="categoryId">
-        <template #default="scope">
-          {{ categoryList.find((item) => item.id === scope.row.categoryId)?.name }}
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="浏览量" min-width="180" prop="browseCount" />
-      <el-table-column align="center" label="作者" min-width="180" prop="author" />
-      <el-table-column align="center" label="文章简介" min-width="250" prop="introduction" />
-      <el-table-column align="center" label="排序" min-width="60" prop="sort" />
-      <el-table-column align="center" label="状态" min-width="60" prop="status">
+      <el-table-column align="center" label="状态" prop="status">
         <template #default="scope">
           <dict-tag :type="DICT_TYPE.COMMON_STATUS" :value="scope.row.status" />
         </template>
       </el-table-column>
+      <el-table-column align="center" label="跳转地址" prop="url" />
       <el-table-column
         :formatter="dateFormatter"
         align="center"
-        label="发布时间"
+        label="创建时间"
         prop="createTime"
         width="180px"
       />
-      <el-table-column align="center" fixed="right" label="操作" width="120">
+      <el-table-column align="center" label="排序" prop="sort" />
+      <el-table-column align="center" label="描述" prop="memo" />
+      <el-table-column align="center" label="操作">
         <template #default="scope">
           <el-button
-            v-hasPermi="['promotion:article:update']"
+            v-hasPermi="['market:banner:update']"
             link
             type="primary"
             @click="openForm('update', scope.row.id)"
@@ -116,7 +95,7 @@
             编辑
           </el-button>
           <el-button
-            v-hasPermi="['promotion:article:delete']"
+            v-hasPermi="['market:banner:delete']"
             link
             type="danger"
             @click="handleDelete(scope.row.id)"
@@ -136,19 +115,17 @@
   </ContentWrap>
 
   <!-- 表单弹窗：添加/修改 -->
-  <ArticleForm ref="formRef" @success="getList" />
+  <BannerForm ref="formRef" @success="getList" />
 </template>
 
 <script lang="ts" setup>
 import { DICT_TYPE, getIntDictOptions } from '@/utils/dict'
 import { dateFormatter } from '@/utils/formatTime'
-import * as ArticleApi from '@/api/mall/promotion/article'
-import ArticleForm from './ArticleForm.vue'
-import * as ArticleCategoryApi from '@/api/mall/promotion/articleCategory'
-import * as ProductSpuApi from '@/api/mall/product/spu'
+import * as BannerApi from '@/api/mall/market/banner'
+import BannerForm from './BannerForm.vue'
 import { createImageViewer } from '@/components/ImageViewer'
 
-defineOptions({ name: 'PromotionArticle' })
+defineOptions({ name: 'Banner' })
 
 const message = useMessage() // 消息弹窗
 const { t } = useI18n() // 国际化
@@ -159,24 +136,24 @@ const list = ref([]) // 列表的数据
 const queryParams = reactive({
   pageNo: 1,
   pageSize: 10,
-  categoryId: undefined,
   title: null,
-  status: undefined,
-  spuId: undefined,
+  status: null,
   createTime: []
 })
 const queryFormRef = ref() // 搜索的表单
+
 /** 文章封面预览 */
 const imagePreview = (imgUrl: string) => {
   createImageViewer({
     urlList: [imgUrl]
   })
 }
+
 /** 查询列表 */
 const getList = async () => {
   loading.value = true
   try {
-    const data = await ArticleApi.getArticlePage(queryParams)
+    const data = await BannerApi.getBannerPage(queryParams)
     list.value = data.list
     total.value = data.total
   } finally {
@@ -208,20 +185,15 @@ const handleDelete = async (id: number) => {
     // 删除的二次确认
     await message.delConfirm()
     // 发起删除
-    await ArticleApi.deleteArticle(id)
+    await BannerApi.deleteBanner(id)
     message.success(t('common.delSuccess'))
     // 刷新列表
     await getList()
   } catch {}
 }
 
-const categoryList = ref<ArticleCategoryApi.ArticleCategoryVO[]>([])
-const spuList = ref<ProductSpuApi.Spu[]>([])
-onMounted(async () => {
-  await getList()
-  // 加载分类、商品列表
-  categoryList.value =
-    (await ArticleCategoryApi.getSimpleArticleCategoryList()) as ArticleCategoryApi.ArticleCategoryVO[]
-  spuList.value = (await ProductSpuApi.getSpuSimpleList()) as ProductSpuApi.Spu[]
+/** 初始化 **/
+onMounted(() => {
+  getList()
 })
 </script>
