1ab5b44254415e050615d91128df0afb703ccc1d
niou233 <2922564446@qq.com>
2023-11-10 11:35:54 +0000

    update src/locales/en.ts.
    Signed-off-by: niou233 <2922564446@qq.com>

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -54,13 +54,7 @@ export default {
     updateTime: 'Update Time',
     copy: 'Copy',
     copySuccess: 'Copy Success',
-    copyError: 'Copy Error',
-    confirmDelStar: 'Delete the Star?',
-    starSuccess: 'Star Success',
-    starFail: 'Star Error',
-    delStarSuccess: 'Del Star Success',
-    delStarFail: 'Del Star Error',
-    existStar: 'exist Star'
+    copyError: 'Copy Error'
   },
   error: {
     noPermission: `Sorry, you don't have permission to access this page.`,
