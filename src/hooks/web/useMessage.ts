2ff97758fd720b281e2b76afa56f2fc637476916
niou233 <2922564446@qq.com>
2023-11-10 11:34:32 +0000

    update src/hooks/web/useMessage.ts.
    Signed-off-by: niou233 <2922564446@qq.com>

--- src/hooks/web/useMessage.ts
+++ src/hooks/web/useMessage.ts
@@ -90,18 +90,6 @@ export const useMessage = () => {
         cancelButtonText: t('common.cancel'),
         type: 'warning'
       })
-    },
-    // 取消收藏窗体
-    delStarConfirm(content?: string, tip?: string) {
-      return ElMessageBox.confirm(
-        content ? content : t('common.confirmDelStar'),
-        tip ? tip : t('common.confirmTitle'),
-        {
-          confirmButtonText: t('common.ok'),
-          cancelButtonText: t('common.cancel'),
-          type: 'warning'
-        }
-      )
     }
   }
 }
