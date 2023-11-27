633965b07dbbf51cdfc8cb57ce54de2a8f3aa56d
Evol郑天 <jpevol@163.com>
2023-11-27 07:07:32 +0000

    同步公众号粉丝接口调用错误
    Signed-off-by: Evol郑天 <jpevol@163.com>

--- src/api/mp/user/index.ts
+++ src/api/mp/user/index.ts
@@ -26,6 +26,6 @@ export const getUserPage = (query) => {
 // 同步公众号粉丝
 export const syncUser = (accountId) => {
   return request.post({
-    url: '/mp/tag/sync?accountId=' + accountId
+    url: '/mp/user/sync?accountId=' + accountId
   })
 }
