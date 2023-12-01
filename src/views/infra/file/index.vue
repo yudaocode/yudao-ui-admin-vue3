a711e96b23d8b3cf4945a395e6dda6b4084a10ef
Marvin <454846659@qq.com>
2023-12-01 09:53:41 +0800

    feat: 跳转到例外一个标签页预览

--- src/views/infra/file/index.vue
+++ src/views/infra/file/index.vue
@@ -70,7 +70,7 @@
             preview-teleported
             fit="cover"
           />
-          <el-link v-else-if="row.type.includes('pdf')" type="primary" :href="row.url" :underline="false">预览</el-link>
+          <el-link v-else-if="row.type.includes('pdf')" type="primary" :href="row.url" :underline="false" target="_blank">预览</el-link>
           <el-link v-else type="primary" download :href="row.url" :underline="false" target="_blank">下载</el-link>
         </template>
       </el-table-column>
