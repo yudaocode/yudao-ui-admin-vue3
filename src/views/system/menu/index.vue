6593c603a25b1b92905840b401921c956e583117
YunaiV <zhijiantianya@gmail.com>
2023-12-02 21:47:20 +0800

    🐛 修复 menu 在 IDEA 报错的问题

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -29,7 +29,7 @@
         >
           <el-option
             v-for="dict in getIntDictOptions(DICT_TYPE.COMMON_STATUS)"
-            :key="dict.value"
+            :key="dict.value as number"
             :label="dict.label"
             :value="dict.value"
           />
