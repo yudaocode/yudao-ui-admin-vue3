5eb30c7e85b74fcbbf0a3ca61378aedfd960c300
YunaiV <zhijiantianya@gmail.com>
2023-12-03 18:11:54 +0800

    🐛 修复 job 在 IDEA 报错的问题

--- src/views/infra/job/logger/JobLogDetail.vue
+++ src/views/infra/job/logger/JobLogDetail.vue
@@ -40,7 +40,7 @@ defineOptions({ name: 'JobLogDetail' })
 
 const dialogVisible = ref(false) // 弹窗的是否展示
 const detailLoading = ref(false) // 表单的加载中
-const detailData = ref({}) // 详情数据
+const detailData = ref({} as JobLogApi.JobLogVO) // 详情数据
 
 /** 打开弹窗 */
 const open = async (id: number) => {
