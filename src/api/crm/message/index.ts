24849335a834c1570dd7f96e80b92deb754f6ac6
dhb52 <dhb52@126.com>
2024-01-14 22:44:25 +0800

    wip: 待办事项

---
+++ src/api/crm/message/index.ts
@@ -0,0 +1,39 @@
+import request from '@/config/axios'
+
+export interface CustomerVO {
+  id?: number
+  name: string
+  industryId: number
+  level: number
+  source: number
+  followUpStatus?: boolean
+  lockStatus?: boolean
+  dealStatus?: boolean
+  mobile: string
+  telephone: string
+  website: string
+  qq: string
+  wechat: string
+  email: string
+  description: string
+  remark: string
+  ownerUserId?: number
+  ownerUserName?: string
+  ownerUserDept?: string
+  roUserIds?: string
+  rwUserIds?: string
+  areaId?: number
+  areaName?: string
+  detailAddress: string
+  contactLastTime?: Date
+  contactNextTime: Date
+  createTime?: Date
+  updateTime?: Date
+  creator?: string
+  creatorName?: string
+}
+
+// 查询客户列表
+export const getTodayCustomerPage = async (params) => {
+  return await request.get({ url: `/crm/message/todayCustomer`, params })
+}
