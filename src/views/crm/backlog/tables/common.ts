361c06bd6f4d530e16414e466fd29f0fa4300627
dhb52 <dhb52@126.com>
2024-02-06 22:58:33 +0800

    feat: crm-backlog 待审核合同

--- src/views/crm/backlog/tables/common.ts
+++ src/views/crm/backlog/tables/common.ts
@@ -17,3 +17,9 @@ export const CONTACT_STATUS = [
   { label: '已逾期', value: 2 },
   { label: '已联系', value: 3 }
 ]
+
+/** 合同审批状态 */
+export const CONTRACT_AUDIT_STATUS = [
+  { label: '已审批', value: 20 },
+  { label: '待审批', value: 10 }
+]
