cd86c08e59512233f4dbfdde804525d18c1cac3f
YunaiV <zhijiantianya@gmail.com>
2024-02-18 19:05:57 +0800

    📖 ERP：增加 ERP 首页的统计

---
+++ src/views/erp/home/components/SummaryCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col gap-2 bg-[var(--el-bg-color-overlay)] p-6">
+    <div class="flex items-center justify-between text-gray-500">
+      <span>{{ title }}</span>
+    </div>
+    <div class="flex flex-row items-baseline justify-between">
+      <CountTo prefix="￥" :end-val="value" :decimals="2" :duration="500" class="text-3xl" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { propTypes } from '@/utils/propTypes'
+
+/** 价格展示 Card */
+defineOptions({ name: 'ErpSummaryCard' })
+
+defineProps({
+  title: propTypes.string.def('').isRequired,
+  value: propTypes.number.def(0).isRequired
+})
+</script>
