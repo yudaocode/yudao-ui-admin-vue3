c16069d25424fac62687f4b489fc647c36d510dd
zhaozian <>
2024-03-08 14:43:33 +0800

    更换logo和系统名

--- src/components/DiyEditor/index.vue
+++ src/components/DiyEditor/index.vue
@@ -171,7 +171,7 @@
       />
       <div class="flex flex-col">
         <el-text>手机扫码预览</el-text>
-        <Qrcode :text="previewUrl" logo="/logo.gif" />
+        <Qrcode :text="previewUrl" logo="/logo.png" />
       </div>
     </div>
   </Dialog>
