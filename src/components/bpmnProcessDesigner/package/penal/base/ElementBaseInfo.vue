624594bb5c6f2ae3d576001c982e7f52497f8c0a
YunaiV <zhijiantianya@gmail.com>
2024-03-24 10:46:37 +0800

    BPM：处理打开 bpmn 设计器时，key 和 name 覆盖更新

--- src/components/bpmnProcessDesigner/package/penal/base/ElementBaseInfo.vue
+++ src/components/bpmnProcessDesigner/package/penal/base/ElementBaseInfo.vue
@@ -139,6 +139,14 @@ const updateBaseInfo = (key) => {
   }
 }
 
+onMounted(() => {
+  // 针对上传的 bpmn 流程图时，需要延迟 1 毫秒的时间，保证 key 和 name 的更新
+  setTimeout(() => {
+    handleKeyUpdate(props.model.key)
+    handleNameUpdate(props.model.name)
+  }, 110)
+})
+
 watch(
   () => props.businessObject,
   (val) => {
