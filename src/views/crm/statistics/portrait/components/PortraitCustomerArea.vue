f5d013187b1ad2ad838addf29fb54a4da86aeda5
YunaiV <zhijiantianya@gmail.com>
2024-04-18 09:28:07 +0800

    crm：code review 销售漏斗

--- src/views/crm/statistics/portrait/components/PortraitCustomerArea.vue
+++ src/views/crm/statistics/portrait/components/PortraitCustomerArea.vue
@@ -106,7 +106,7 @@ const loadData = async () => {
   areaStatisticsList.value = areaList.map((item: CrmStatisticCustomerAreaRespVO) => {
     return {
       ...item,
-      areaName: item.areaName
+      areaName: item.areaName // TODO @puhui999：这里最好注释下原因哈, 🤣 我从 mall copy 过来的；这块看着是适合 ercharts 的地名，要不抽个小的 js 方法，然后把涉及到的地方都替换掉。
       // .replace('维吾尔自治区', '')
       // .replace('壮族自治区', '')
       // .replace('回族自治区', '')
