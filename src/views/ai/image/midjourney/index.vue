12e87cc0f26fa00354c2778aa89fee83806c3d66
cherishsince <cherishsince@aliyun.com>
2024-05-25 21:18:43 +0800

    【增加】Ai Image midjourney 增加，提示词、模型、尺寸

--- src/views/ai/image/midjourney/index.vue
+++ src/views/ai/image/midjourney/index.vue
@@ -1,10 +1,283 @@
-<!-- midjourney -->
+<!-- dall3 -->
 <template>
-  midjourney
+  <div class="prompt">
+    <el-text tag="b">画面描述</el-text>
+    <el-text tag="p">建议使用“形容词+动词+风格”的格式，使用“，”隔开.</el-text>
+    <el-input
+      v-model="prompt"
+      maxlength="1024"
+      rows="5"
+      style="width: 100%; margin-top: 15px;"
+      input-style="border-radius: 7px;"
+      placeholder="例如：童话里的小屋应该是什么样子？"
+      show-word-limit
+      type="textarea"
+    />
+  </div>
+  <div class="hot-words">
+    <div>
+      <el-text tag="b">随机热词</el-text>
+    </div>
+    <el-space wrap class="word-list">
+      <el-button round
+                 class="btn"
+                 :type="(selectHotWord === hotWord ? 'primary' : 'default')"
+                 v-for="hotWord in hotWords"
+                 :key="hotWord"
+                 @click="handlerHotWordClick(hotWord)"
+      >
+        {{ hotWord }}
+      </el-button>
+    </el-space>
+  </div>
+  <div class="model">
+    <div>
+      <el-text tag="b">模型</el-text>
+    </div>
+    <el-space wrap class="model-list">
+      <div
+        :class="selectModel === model ? 'modal-item selectModel' : 'modal-item'"
+        v-for="model in models"
+        :key="model.key"
+
+      >
+        <el-image
+          :src="model.image"
+          fit="contain"
+          @click="handlerModelClick(model)"
+        />
+        <div class="model-font">{{model.name}}</div>
+      </div>
+    </el-space>
+  </div>
+  <div class="image-size">
+    <div>
+      <el-text tag="b">尺寸</el-text>
+    </div>
+    <el-space wrap class="size-list">
+      <div class="size-item"
+           v-for="imageSize in imageSizeList"
+           :key="imageSize.key"
+           @click="handlerSizeClick(imageSize)">
+        <div :class="selectImageSize === imageSize ? 'size-wrapper selectImageSize' : 'size-wrapper'">
+          <div :style="imageSize.style"></div>
+        </div>
+        <div class="size-font">{{ imageSize.key }}</div>
+      </div>
+    </el-space>
+  </div>
+  <div class="btns">
+    <!--    <el-button size="large" round>重置内容</el-button>-->
+    <el-button type="primary" size="large" round @click="handlerGenerateImage">生成内容</el-button>
+  </div>
 </template>
 <script setup lang="ts">
 
+// image 模型
+interface ImageModelVO {
+  key: string
+  name: string
+  image: string
+}
+
+// image 大小
+interface ImageSizeVO {
+  key: string
+  style: string,
+}
+
+// 定义属性
+const prompt = ref<string>('')  // 提示词
+const selectHotWord = ref<string>('') // 选中的热词
+const hotWords = ref<string[]>(['中国旗袍', '古装美女', '卡通头像', '机甲战士', '童话小屋', '中国长城'])  // 热词
+const selectModel = ref<any>() // 选中的热词
+const models = ref<ImageModelVO[]>([
+  {
+    key: 'qinxi',
+    name: 'MJ',
+    image: 'https://h5.cxyhub.com/images/mj.jpeg',
+  },
+  {
+    key: 'ziran',
+    name: 'NIJI',
+    image: 'https://h5.cxyhub.com/images/niji.jpeg',
+  },
+])  // 模型
+
+const selectImageSize = ref<ImageSizeVO>({} as ImageSizeVO) // 选中 size
+const imageSizeList = ref<ImageSizeVO[]>([
+  {
+    key: '1:1',
+    style: 'width: 30px; height: 30px;background-color: #dcdcdc;',
+  },
+  {
+    key: '3:4',
+    style: 'width: 30px; height: 40px;background-color: #dcdcdc;',
+  },
+  {
+    key: '4:3',
+    style: 'width: 40px; height: 30px;background-color: #dcdcdc;',
+  },
+  {
+    key: '9:16',
+    style: 'width: 30px; height: 50px;background-color: #dcdcdc;',
+  },
+  {
+    key: '16:9',
+    style: 'width: 50px; height: 30px;background-color: #dcdcdc;',
+  },
+]) // size
+
+// 定义 Props
+const props = defineProps({})
+
+/**
+ * 热词 - click
+ */
+const handlerHotWordClick = async (hotWord: string) => {
+  // 取消
+  if (selectHotWord.value == hotWord) {
+    selectHotWord.value = ''
+    return
+  }
+  // 选中
+  selectHotWord.value = hotWord
+}
+
+/**
+ * 模型 - click
+ */
+const handlerModelClick = async (model: ImageModelVO) => {
+  if (selectModel.value === model) {
+    selectModel.value = {} as ImageModelVO
+    return
+  }
+  selectModel.value = model
+}
+
+/**
+ * size - click
+ */
+const handlerSizeClick = async (imageSize: ImageSizeVO) => {
+  if (selectImageSize.value === imageSize) {
+    selectImageSize.value = {} as ImageSizeVO
+    return
+  }
+  selectImageSize.value = imageSize
+  console.log(imageSize)
+}
+
+/**
+ * 图片生产
+ */
+const handlerGenerateImage = async () => {
+  // todo @范 图片生产逻辑
+}
 </script>
 <style scoped lang="scss">
 
+// 提示词
+.prompt {
+}
+
+// 热词
+.hot-words {
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+
+  .word-list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: start;
+    margin-top: 15px;
+
+    .btn {
+      margin: 0;
+    }
+  }
+}
+
+// 模型
+.model {
+  margin-top: 30px;
+
+  .model-list {
+    margin-top: 15px;
+
+    .modal-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 110px;
+      //outline: 1px solid blue;
+      overflow: hidden;
+      border: 3px solid transparent;
+      cursor: pointer;
+
+      .model-font {
+        font-size: 14px;
+        color: #3e3e3e;
+        font-weight: bold;
+      }
+    }
+
+    .selectModel {
+      border: 3px solid #1293ff;
+      border-radius: 5px;
+    }
+  }
+}
+
+
+// 尺寸
+.image-size {
+  width: 100%;
+  margin-top: 30px;
+
+  .size-list {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 20px;
+
+    .size-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      cursor: pointer;
+
+      .size-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border-radius: 7px;
+        padding: 4px;
+        width: 50px;
+        height: 50px;
+        background-color: #fff;
+        border: 1px solid #fff;
+      }
+
+      .size-font {
+        font-size: 14px;
+        color: #3e3e3e;
+        font-weight: bold;
+      }
+    }
+  }
+
+  .selectImageSize {
+    border: 1px solid #1293ff !important;
+  }
+}
+
+.btns {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
 </style>
