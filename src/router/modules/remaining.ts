a7c37fa29568722ea631561f5e0e193b02194887
YunaiV <zhijiantianya@gmail.com>
2024-08-10 19:13:02 +0800

    【功能修复】绘图作品，一直打开的问题

--- src/router/modules/remaining.ts
+++ src/router/modules/remaining.ts
@@ -589,8 +589,7 @@ const remainingRouter: AppRouteRecordRaw[] = [
         meta: {
           title: '绘图作品',
           icon: 'ep:home-filled',
-          noCache: false,
-          affix: true
+          noCache: false
         }
       }
     ]
