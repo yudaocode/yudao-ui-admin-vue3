c6b6d2f4043232e9d3f6a93f3a33d5197d84588e
痴货 <252048765@qq.com>
2024-10-08 16:14:05 +0800

    【功能优化】优化门店店员功能

--- src/views/mall/trade/delivery/pickUpStore/components/StoreStaffTableSelect.vue
+++ src/views/mall/trade/delivery/pickUpStore/components/StoreStaffTableSelect.vue
@@ -160,7 +160,7 @@ const queryParams = reactive({
   mobile: undefined,
   status: undefined,
   deptId: undefined,
-  roleId: 4,
+  roleId: 5,
   createTime: []
 })
 const queryFormRef = ref() // 搜索的表单
