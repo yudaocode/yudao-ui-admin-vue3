dad97e2f172781feb56eaf2c185c5bc263e6355e
YunaiV <zhijiantianya@gmail.com>
2024-10-12 20:09:01 +0800

    【修复】工作流：流程实例的查询界面，缺少搜索、重置按钮

--- src/views/bpm/processInstance/manager/index.vue
+++ src/views/bpm/processInstance/manager/index.vue
@@ -79,6 +79,10 @@
           class="!w-240px"
         />
       </el-form-item>
+      <el-form-item>
+        <el-button @click="handleQuery"><Icon icon="ep:search" class="mr-5px" /> 搜索</el-button>
+        <el-button @click="resetQuery"><Icon icon="ep:refresh" class="mr-5px" /> 重置</el-button>
+      </el-form-item>
     </el-form>
   </ContentWrap>
 
