7474c6b83be9e69e60f1731abd82177919f61e5d
LesanOuO <1960681385@qq.com>
2024-12-07 10:45:47 +0800

    fix: unplugin-vue-components报错，同名UserTask

--- src/components/bpmnProcessDesigner/package/penal/custom-config/data.ts
+++ src/components/bpmnProcessDesigner/package/penal/custom-config/data.ts
@@ -1,10 +1,10 @@
-import UserTask from './components/UserTask.vue'
+import UserTaskCustomConfig from './components/UserTaskCustomConfig.vue'
 import BoundaryEventTimer from './components/BoundaryEventTimer.vue'
 
 export const CustomConfigMap = {
   UserTask: {
     name: '用户任务',
-    componet: UserTask
+    componet: UserTaskCustomConfig
   },
   BoundaryEventTimerEventDefinition: {
     name: '定时边界事件(非中断)',
