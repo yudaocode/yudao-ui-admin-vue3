3e826776f466b5e932fca6c55f584f03aaf5c804
Lesan <1960681385@qq.com>
2024-12-11 13:46:09 +0800

    fix: 属性命名错误

--- src/components/bpmnProcessDesigner/package/penal/task/data.ts
+++ src/components/bpmnProcessDesigner/package/penal/task/data.ts
@@ -6,19 +6,19 @@ import ReceiveTask from './task-components/ReceiveTask.vue'
 export const installedComponent = {
   UserTask: {
     name: '用户任务',
-    componet: UserTask
+    component: UserTask
   },
   ServiceTask: {
     name: '服务任务',
-    componet: ServiceTask
+    component: ServiceTask
   },
   ScriptTask: {
     name: '脚本任务',
-    componet: ScriptTask
+    component: ScriptTask
   },
   ReceiveTask: {
     name: '接收任务',
-    componet: ReceiveTask
+    component: ReceiveTask
   }
 }
 
