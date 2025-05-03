93056fa57b6921fa34c254e9b8f5e8217db788b0
YunaiV <zhijiantianya@gmail.com>
2025-05-03 10:26:27 +0800

    feat：集成积木仪表盘

---
+++ src/views/report/jmreport/bi.vue
@@ -0,0 +1,15 @@
+<template>
+  <doc-alert title="大屏设计器" url="https://doc.iocoder.cn/screen/" />
+
+  <ContentWrap :bodyStyle="{ padding: '0px' }" class="!mb-0">
+    <IFrame :src="src" />
+  </ContentWrap>
+</template>
+<script lang="ts" setup>
+import { getRefreshToken } from '@/utils/auth'
+
+defineOptions({ name: 'JimuBI' })
+
+// 使用 getRefreshToken() 方法，而不使用 getAccessToken() 方法的原因：积木报表无法方便的刷新访问令牌
+const src = ref(import.meta.env.VITE_BASE_URL + '/drag/list?token=' + getRefreshToken())
+</script>
