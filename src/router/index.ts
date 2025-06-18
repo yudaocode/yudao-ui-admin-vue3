90f2f52f2495f37454b6d71c44660638e92ca950
tanghuting <1286545749@qq.com>
2025-06-18 15:25:34 +0800

    fix(router): 更新重置路由的白名单名称
    将 NoFind 改为 NoFound 并添加 Home 到白名单中，以保持路由名称一致性

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const router = createRouter({
 })
 
 export const resetRouter = (): void => {
-  const resetWhiteNameList = ['Redirect', 'Login', 'NoFind', 'Root']
+  const resetWhiteNameList = ['Redirect', 'Login', 'NoFound', 'Home']
   router.getRoutes().forEach((route) => {
     const { name } = route
     if (name && !resetWhiteNameList.includes(name as string)) {
