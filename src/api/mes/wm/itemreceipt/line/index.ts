6fd1806c4c4367e6fb828849b725224ca8de8e34
YunaiV <zhijiantianya@gmail.com>
2026-02-22 15:43:58 +0800

    feat(mes): add request and response VO for MES item receipt and detail, including service and controller implementations

---
+++ src/api/mes/wm/itemreceipt/line/index.ts
@@ -0,0 +1,57 @@
+import request from '@/config/axios'
+
+// MES 采购入库单行 VO
+export interface WmItemReceiptLineVO {
+  id: number
+  receiptId: number
+  noticeLineId: number
+  itemId: number
+  itemCode: string
+  itemName: string
+  specification: string
+  unitMeasureName: string
+  receivedQuantity: number
+  batchId: number
+  warehouseId: number
+  warehouseName: string
+  locationId: number
+  locationName: string
+  areaId: number
+  areaName: string
+  productionDate: string
+  expireDate: string
+  productionBatchNumber: string
+  iqcCheckFlag: boolean
+  iqcId: number
+  iqcCode: string
+  remark: string
+  createTime: string
+}
+
+// MES 采购入库单行 API
+export const WmItemReceiptLineApi = {
+  // 查询采购入库单行分页
+  getItemReceiptLinePage: async (params: any) => {
+    return await request.get({ url: '/mes/wm/item-receipt-line/page', params })
+  },
+
+  // 查询采购入库单行详情
+  getItemReceiptLine: async (id: number) => {
+    return await request.get({ url: '/mes/wm/item-receipt-line/get?id=' + id })
+  },
+
+  // 新增采购入库单行
+  createItemReceiptLine: async (data: WmItemReceiptLineVO) => {
+    return await request.post({ url: '/mes/wm/item-receipt-line/create', data })
+  },
+
+  // 修改采购入库单行
+  updateItemReceiptLine: async (data: WmItemReceiptLineVO) => {
+    return await request.put({ url: '/mes/wm/item-receipt-line/update', data })
+  },
+
+  // 删除采购入库单行
+  deleteItemReceiptLine: async (id: number) => {
+    return await request.delete({ url: '/mes/wm/item-receipt-line/delete?id=' + id })
+  }
+}
