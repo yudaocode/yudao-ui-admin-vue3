0b29aaadaf3d52700fb4fba6385ed5585130e6fe
YunaiV <zhijiantianya@gmail.com>
2026-02-28 13:28:27 +0800

    ✨ feat(mes): 添加生产退料单及明细相关功能 新增生产退料单、退料单行及退料明细的请求和响应对象，完善相关的服务和映射接口，支持生产退料单的创建、更新和查询功能。

---
+++ src/api/mes/wm/returnissue/index.ts
@@ -0,0 +1,75 @@
+import request from '@/config/axios'
+
+// MES 生产退料单 VO
+export interface WmReturnIssueVO {
+  id?: number
+  code?: string
+  name: string
+  workstationId?: number
+  workstationName?: string
+  workOrderId?: number
+  workOrderCode?: string
+  returnType?: string
+  returnDate?: string
+  status?: number
+  remark?: string
+  createTime?: string
+}
+
+// MES 生产退料单 API
+export const WmReturnIssueApi = {
+  // 查询生产退料单分页
+  getReturnIssuePage: async (params: any) => {
+    return await request.get({ url: '/mes/wm/return-issue/page', params })
+  },
+
+  // 查询生产退料单详情
+  getReturnIssue: async (id: number) => {
+    return await request.get({ url: '/mes/wm/return-issue/get?id=' + id })
+  },
+
+  // 新增生产退料单
+  createReturnIssue: async (data: WmReturnIssueVO) => {
+    return await request.post({ url: '/mes/wm/return-issue/create', data })
+  },
+
+  // 修改生产退料单
+  updateReturnIssue: async (data: WmReturnIssueVO) => {
+    return await request.put({ url: '/mes/wm/return-issue/update', data })
+  },
+
+  // 删除生产退料单
+  deleteReturnIssue: async (id: number) => {
+    return await request.delete({ url: '/mes/wm/return-issue/delete?id=' + id })
+  },
+
+  // 提交生产退料单
+  submitReturnIssue: async (id: number) => {
+    return await request.put({ url: '/mes/wm/return-issue/submit?id=' + id })
+  },
+
+  // 入库上架
+  stockReturnIssue: async (id: number) => {
+    return await request.put({ url: '/mes/wm/return-issue/stock?id=' + id })
+  },
+
+  // 取消生产退料单
+  cancelReturnIssue: async (id: number) => {
+    return await request.put({ url: '/mes/wm/return-issue/cancel?id=' + id })
+  },
+
+  // 完成生产退料单
+  finishReturnIssue: async (id: number) => {
+    return await request.put({ url: '/mes/wm/return-issue/finish?id=' + id })
+  },
+
+  // 校验生产退料单数量（每行明细数量之和是否等于行退料数量）
+  checkReturnIssueQuantity: async (id: number) => {
+    return await request.get({ url: '/mes/wm/return-issue/check-quantity?id=' + id })
+  },
+
+  // 导出生产退料单 Excel
+  exportReturnIssue: async (params: any) => {
+    return await request.download({ url: '/mes/wm/return-issue/export-excel', params })
+  }
+}
---
+++ src/api/mes/wm/returnissue/line/index.ts
@@ -0,0 +1,46 @@
+import request from '@/config/axios'
+
+// MES 生产退料单行 VO
+export interface WmReturnIssueLineVO {
+  id?: number
+  issueId: number
+  itemId: number
+  itemCode?: string
+  itemName?: string
+  specification?: string
+  unitMeasureName?: string
+  quantity: number
+  batchId?: number
+  qcFlag?: boolean
+  qualityStatus?: number
+  ipqcId?: number
+  remark?: string
+}
+
+// MES 生产退料单行 API
+export const WmReturnIssueLineApi = {
+  // 查询生产退料单行分页
+  getReturnIssueLinePage: async (params: any) => {
+    return await request.get({ url: '/mes/wm/return-issue-line/page', params })
+  },
+
+  // 查询生产退料单行详情
+  getReturnIssueLine: async (id: number) => {
+    return await request.get({ url: '/mes/wm/return-issue-line/get?id=' + id })
+  },
+
+  // 新增生产退料单行
+  createReturnIssueLine: async (data: WmReturnIssueLineVO) => {
+    return await request.post({ url: '/mes/wm/return-issue-line/create', data })
+  },
+
+  // 修改生产退料单行
+  updateReturnIssueLine: async (data: WmReturnIssueLineVO) => {
+    return await request.put({ url: '/mes/wm/return-issue-line/update', data })
+  },
+
+  // 删除生产退料单行
+  deleteReturnIssueLine: async (id: number) => {
+    return await request.delete({ url: '/mes/wm/return-issue-line/delete?id=' + id })
+  }
+}
