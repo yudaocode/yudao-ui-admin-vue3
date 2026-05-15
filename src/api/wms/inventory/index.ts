5f944548a3df8f31da7eb0499fcc403f7039a8d0
YunaiV <zhijiantianya@gmail.com>
2026-05-15 09:46:40 +0800

    fix(wms): 调整文档的最新内容

--- src/api/wms/inventory/index.ts
+++ src/api/wms/inventory/index.ts
@@ -22,5 +22,10 @@ export const InventoryApi = {
   // 查询库存统计分页
   getInventoryPage: async (params: any) => {
     return await request.get({ url: '/wms/inventory/page', params })
+  },
+
+  // 查询指定仓库的库存统计列表
+  getInventoryListByWarehouseId: async (warehouseId: number) => {
+    return await request.get({ url: '/wms/inventory/list-by-warehouse-id', params: { warehouseId } })
   }
 }
