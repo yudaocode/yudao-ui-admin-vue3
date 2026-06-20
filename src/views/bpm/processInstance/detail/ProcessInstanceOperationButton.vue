026f7e9b1090adcd07ed428687a32c48b3b58229
jason <2667446@qq.com>
2026-06-20 22:22:16 +0800

    fix: [bpm] 并行分支后的审批人自选问题修复,https://t.zsxq.com/daxv1

--- src/views/bpm/processInstance/detail/ProcessInstanceOperationButton.vue
+++ src/views/bpm/processInstance/detail/ProcessInstanceOperationButton.vue
@@ -816,7 +816,8 @@ const initNextAssigneesFormField = async () => {
           isEmpty(node.candidateUsers) &&
           CandidateStrategy.START_USER_SELECT === node.candidateStrategy) ||
         // 情况二：当前节点是审批人自选
-        CandidateStrategy.APPROVE_USER_SELECT === node.candidateStrategy
+        (isEmpty(node.candidateUsers) &&
+        CandidateStrategy.APPROVE_USER_SELECT === node.candidateStrategy)
       ) {
         nextAssigneesActivityNode.value.push(node)
       }
@@ -849,7 +850,7 @@ const validateNextAssignees = () => {
   }
   // 如果需要自选审批人，则校验每个节点是否都已配置审批人
   for (const item of nextAssigneesActivityNode.value) {
-    if (isEmpty(approveReasonForm.nextAssignees[item.id])) {
+    if (isEmpty(item.candidateUsers) && isEmpty(approveReasonForm.nextAssignees[item.id])) {
       message.warning('下一个节点的审批人不能为空!')
       return false
     }
